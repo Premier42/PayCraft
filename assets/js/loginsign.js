cc5bb782f52835bc1b4728bdab8d49e00303eaf5
shinzuu <not.shinzuu@gmail.com>
2024-02-11 15:59:48 +0600

    minor changes

---
+++ assets/js/loginsign.js
@@ -0,0 +1,7 @@
+//author shinzuu
+// sign up script
+document.getElementById("signupForm").addEventListener("submit", function(event){
+    event.preventDefault(); // Prevents the form from submitting normally
+    // Your JavaScript code to run when the submit button is clicked goes here
+    alert("Form submitted!");
+});
